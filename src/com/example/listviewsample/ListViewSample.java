45cbec7d18eaacdf24cc2970e202b61209c21082
hoshit <hoshi@gclue.jp>
2013-04-04 08:48:54 +0900

    初回コミット.

--- src/com/example/listviewsample/ListViewSample.java
+++ src/com/example/listviewsample/ListViewSample.java
@@ -2,22 +2,23 @@ package com.example.listviewsample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class ListViewSample extends Activity {
 	
 	/**
 	 * ListView
 	 */
-	ListView mListView;
-	
+	private ListView mListView;
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		
 		// Adapterの作成
 		ArrayAdapter<String> mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
 		
@@ -40,5 +41,14 @@ public class ListViewSample extends Activity {
 		
 		// アダプターをセット
 		mListView.setAdapter(mAdapter);
+		
+		// クリックされた時のイベントを追加
+		mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				String selectList = (String) parent.getItemAtPosition(position);
+				Toast.makeText(ListViewSample.this, "pos="+position+":"+selectList, Toast.LENGTH_LONG).show();
+			}
+		});
 	}
 }
